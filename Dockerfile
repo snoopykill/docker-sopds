e258aee2f2ddb965cb4c734912f980896b601626
Andrei Dzenisenia <andrei_dzenisenia@epam.com>
2023-09-07 09:08:50 +0200

    mysql: add support for mysql/mariadb 4

--- Dockerfile
+++ Dockerfile
@@ -12,11 +12,13 @@ RUN apt-get -y update \
 COPY requirements.txt .
 COPY configs/settings.py ./sopds
 
-RUN cp /usr/share/zoneinfo/Europe/Moscow /etc/localtime \
-    && echo "Europe/Moscow" > /etc/timezone \
+RUN cp /usr/share/zoneinfo/Europe/Warsaw /etc/localtime \
+    && echo "Europe/Warsaw" > /etc/timezone \
     && pip3 install --upgrade -r requirements.txt \
     && mkdir -p /sopds/tmp/ \
     && chmod ugo+w /sopds/tmp/
+
+ENV TIME_ZONE="Europe/Warsaw"
 
 COPY scripts/start.sh /start.sh
 RUN chmod +x /start.sh
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+DB
+cd /sopds
+if [ $MIGRATE == True ]; then
 python3 manage.py migrate
+fi
+if [ $DBCLEAR == True ]; then
 python3 manage.py sopds_util clear
-python3 manage.py sopds_scanner start --daemon
+fi
+#autocreate the superuser
+if [[ ! -z $SOPDS_SU_NAME && ! -z $SOPDS_SU_EMAIL &&  ! -z $SOPDS_SU_PASS ]]; then
+expect /sopds/superuser.exp
+fi
+python3 manage.py sopds_util setconf SOPDS_AUTH False
+python3 manage.py sopds_util setconf SOPDS_SCAN_SHED_HOUR "11"
+# python3 manage.py sopds_util setconf SOPDS_LANGUAGE ru-RU
 python3 manage.py sopds_util setconf SOPDS_SCAN_START_DIRECTLY True
+python3 manage.py sopds_util setconf SOPDS_INPX_ENABLE True
+python3 manage.py sopds_server start & python3 manage.py sopds_scanner start
